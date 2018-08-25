87fb961c6ea0f5b23bbd29e75d75b6bb57ebf2eb
Justin Massey <jmassey_1991@hotmail.com>
2018-08-25 14:13:01 -0400

    setting up project structure

--- Project1Name/src/util/connection-util.ts
+++ Project1Name/src/util/connection-util.ts
@@ -2,7 +2,7 @@ import { Pool, Client } from 'pg';
 
 export const connectionPool = new Pool({
   user: process.env["1808_MOVIE_DB_USERNAME"],
-  host: 'localhost',
+  host: 'revature-1808.cxks8zic54ol.us-east-1.rds.amazonaws.com',
   database: 'postgres',
   password: process.env["1808_MOVIE_DB_PASSWORD"],
   port: 5432,
